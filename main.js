9185c3bcb28595b834aceafc6f380afc4f2eaf48
Emily Phee <ephee@uci.edu>
2022-05-31 21:37:49 -0700

    CSS: Change background and main color, JS: Scroll to section

--- main.js
+++ main.js
@@ -11,3 +11,72 @@ document.addEventListener('scroll', () => {
     navBar.classList.remove('navbar--dark');
   }
 });
+
+const homeContactBtn = document.querySelector('.home__contact');
+const navbarMenuItems = document.querySelectorAll('.navbar__menu__item');
+
+// Sections
+const home = document.getElementById('home');
+const about = document.getElementById('about');
+const skills = document.getElementById('skills');
+const work = document.getElementById('work');
+const contact = document.getElementById('contact');
+
+homeContactBtn.addEventListener('click', () => {
+  contact.scrollIntoView();
+});
+
+navbarMenuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    removeClassName();
+    item.classList.add('active');
+
+    // check which section should go
+    const menuName = item.innerHTML.toLowerCase();
+    if (menuName.includes(home.id)) {
+      home.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest',
+      });
+    }
+
+    if (menuName.includes(about.id)) {
+      about.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest',
+      });
+    }
+
+    if (menuName.includes(skills.id)) {
+      skills.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest',
+      });
+    }
+
+    if (menuName.includes(work.id)) {
+      work.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest',
+      });
+    }
+
+    if (menuName.includes(contact.id)) {
+      contact.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest',
+      });
+    }
+  });
+});
+
+const removeClassName = () => {
+  navbarMenuItems.forEach(item => {
+    item.classList.remove('active');
+  });
+};
